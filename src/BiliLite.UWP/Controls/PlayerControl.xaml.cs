0b5c3316a9c3314439abf86dff9471f257a6f480
iloxmn <tk_@live.com>
2022-09-20 16:22:57 +0800

    Fix Danmu

--- src/BiliLite.UWP/Controls/PlayerControl.xaml.cs
+++ src/BiliLite.UWP/Controls/PlayerControl.xaml.cs
@@ -2518,6 +2518,12 @@ namespace BiliLite.Controls
                 }
             }
         }
+        private void Player_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            RectangleGeometry rectangle = new RectangleGeometry();
+            rectangle.Rect = new Rect(0, 0, SplitView.ActualWidth, SplitView.ActualHeight);
+            DanmuControl.Clip = rectangle;
+        }
     }
     public class CompareDanmakuModel : IEqualityComparer<DanmakuModel>
     {
